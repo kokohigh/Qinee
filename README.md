af9175634f89df8e1dd06e1d35180e5801ef9a00
kokohigh <kokohigh@iCloud.com>
2023-10-06 19:04:09 +0900

    Clarify the inheritance relationship

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Qinee
 A test project for solidity
+
+冷启动问题：
+从组织的成立角度，一般开始时都是由一个核心集团启动项目，并在运行中逐渐淡化核心的控制权，因此采用MultiOwner作为项目的开始
+需要为增加，删除Owner增加一个决策方式
+
+继承关系：
+MultiOwner          WCB     CB      SFSet
+    ｜
+VersionController
+    ｜
+DataStorage
+
+可更新能力：
+可以由一个合约调用另一个合约的方法，如果被调用的方法是一个Factor方法，则可以生成对应的智能合约（结算业务）
+通过将同名却不同版本的SFSet部署在不同地址，用SFSet的地址控制实际部署的结算业务
--- contracts/VersionController.sol
+++ contracts/VersionController.sol
@@ -1,18 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
-import "./DataStorage.sol";
+import "./MultiOwnership.sol";
 
-contract VersionController{
-    address internal version;
-    
-    event logUpdateVersion(uint timestamp, address version);
-    modifier newVersionOnly(address _ver){
+contract VersionController is MultiOwnership {
+    uint internal version;
+
+    event logUpdateVersion(uint timestamp, uint version);
+
+    modifier newVersionOnly(uint _ver) virtual {
         require(version == _ver, "Vension conflict.");
         _;
     }
 
-    function updateVersion(address _newVer) public{
+    constructor(address[] memory _owners) MultiOwnership(_owners) {}
+
+    function updateVersion(uint _newVer) public {
         version = _newVer;
         emit logUpdateVersion(block.timestamp, _newVer);
+    }
+
+    function getVersion() external view returns (uint) {
+        return version;
     }
 }
